7195f648d5195c1eaa8503358b9b55c4e3b1cc89
Vladyslav Antoniv <vladyslavantoniv@gmail.com>
2026-01-28 17:28:11 +0200

    added favoutites page

---
+++ app/[locale]/(shop)/favourites/page.tsx
@@ -0,0 +1,5 @@
+import { FavouritesPage } from '@/pages/FavouritesPage';
+
+export default function Page() {
+ return <FavouritesPage />;
+}
--- src/0-pages/FavouritesPage.tsx
+++ src/0-pages/FavouritesPage.tsx
@@ -1,3 +1,37 @@
+"use client";
+
+import { useAppSelector } from '@/shared/api/lib/hooks';
+import { Layout } from '@/entities/product/ui/Layout';
+
 export function FavouritesPage() {
-  return <div>Favourites Page</div>;
+  const { favouritesProducts } = useAppSelector((state) => state.favourites);
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      
+      <section className="mb-8">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-3xl md:text-4xl font-extrabold text-gray-900 tracking-tight">
+            Favourites
+          </h1>
+          
+          <span className="text-sm text-gray-500 font-medium ml-1">
+            {favouritesProducts.length} models
+          </span>
+        </div>
+      </section>
+
+      <section>
+        {favouritesProducts.length > 0 ? (
+          <Layout products={favouritesProducts} />
+        ) : (
+          <div className="flex flex-col items-center justify-center py-20 text-center">
+            <p className="text-gray-500 text-lg">You haven't added any favourites yet.</p>
+          </div>
+        )}
+      </section>
+
+    </div>
+  );
 }
+
